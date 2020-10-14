fe28a951a94a8e6f64f839b9bd79ddeadd15e6f5
Andrew Worley <andrew.worley@johnlewis.co.uk>
2020-10-14 18:10:46 +0100

    Make a Yoda store, all non-dynamic currently

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -1,10 +1,43 @@
+import dev.fritz2.binding.Handler
+import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.const
+import dev.fritz2.binding.handledBy
 import dev.fritz2.dom.html.render
 import dev.fritz2.dom.mount
+import dev.fritz2.dom.values
+import kotlinx.coroutines.flow.map
+
+data class YodaData(
+    val input: String = "",
+    val yodaSpeak: String = "Curious, you are"
+)
+
+object YodaStore : RootStore<YodaData>(YodaData()) {
+    val updateInput: Handler<String> = handle { yodaData, newInput ->
+        yodaData.copy(input = newInput)
+    }
+}
 
 fun main() {
     render {
-        p {
-            text("Hello World!")
+        div {
+            div {
+                label {
+                    text("Please enter some text:")
+                }
+                input("text-input") {
+                    placeholder = const("Please enter some text:")
+                    type = const("text")
+
+                    changes.values() handledBy YodaStore.updateInput
+                }
+            }
+            button {
+                text("Translate to Yoda!")
+            }
+            p {
+                YodaStore.data.map { it.yodaSpeak }.bind()
+            }
         }
     }.mount("target")
 }
