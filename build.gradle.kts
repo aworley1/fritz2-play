9835b6f7ada50043e8957d1ea83a8084f486dc1c
Andrew Worley <aworley1@googlemail.com>
2020-10-14 16:59:15 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform") version "1.4.0"
+    id("dev.fritz2.fritz2-gradle") version "0.7.1"
+}
+
+repositories {
+    jcenter()
+}
+
+kotlin {
+    kotlin {
+        jvm()
+        js().browser()
+
+        sourceSets {
+            val commonMain by getting {
+                dependencies {
+                    implementation(kotlin("stdlib"))
+                }
+            }
+            val jvmMain by getting {
+                dependencies {
+                }
+            }
+            val jsMain by getting {
+                dependencies {
+                }
+            }
+        }
+    }
+}
